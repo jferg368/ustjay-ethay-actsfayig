86b4dffab5d86aad2a574b078d5d97445b798d7c
Jay Ferguson <fergujo@amazon.com>
2020-05-02 19:06:53 -0700

    Fixed main.py.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 
 import requests
-from flask import Flask, send_file, Response
+from flask import Flask, send_file, Response, render_template
 from bs4 import BeautifulSoup
 
 app = Flask(__name__)
@@ -14,9 +14,17 @@ def get_fact():
     return (facts[0].getText().strip())
 
 
+def pig_latinize(fact):
+    response = requests.post(
+        "https://hidden-journey-62459.herokuapp.com/piglatinize/",
+        {'input_text': fact}
+    )
+    return response.url
+
+
 @app.route('/')
 def home():
-    url = pig_latinize(get_fact())[0]
+    url = pig_latinize(get_fact())
     return render_template('homepage.jinja2', url=url)
 
 
